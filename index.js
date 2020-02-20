976f81002280e6357c682aee7e8badfdc54cb325
masonmeier <57198981+masonmeier@users.noreply.github.com>
2020-02-19 16:34:42 -0800

    First commit

---
+++ index.js
@@ -0,0 +1,54 @@
+  
+'use strict'
+
+function formatQueryParams(params) {
+    const queryItems = Object.keys(params).map(key => `${[encodeURIComponent(key)]}=${encodeURIComponent(params[key])}`);
+    return queryItems.join('&');
+}
+
+function displayResults(responseJson, maxResults) {
+    console.log(responseJson);
+    $('.js-error-message').empty();
+    $('.list').empty();
+    for (let i = 0; i < responseJson.data.length & i < maxResults; i++) {
+        $('.list').append(`<li><h3><a href="${responseJson.data[i].url}">${responseJson.data[i].fullName}</a></h3>
+        <p>${responseJson.data[i].description}</p>
+        </li>`);
+    }
+    $('.results').removeClass('hidden');
+}
+
+function getParks(baseUrl, stateArr, maxResults, apiKey) {
+    const params = {
+        stateCode: stateArr,
+        limit: maxResults
+    }
+    const queryString = formatQueryParams(params);
+    const url = baseUrl + '?' + queryString + '&api_key=' + apiKey;
+    console.log(url);
+
+    fetch(url)
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson, maxResults))
+    .catch(err => {
+        $('.js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function watchForm() {
+    $('.js-form').on('submit', function() {
+        event.preventDefault();
+        const baseUrl = 'https://api.nps.gov/api/v1/parks'
+        const stateArr = $('#term').val().split(",");
+        const maxResults = $('#max-results').val();
+        const apiKey = 'Gmc8uW58zqLayjaA7sdUUdtMXKib9eBUX9bQl6Wb'
+        getParks(baseUrl, stateArr, maxResults, apiKey);
+    })
+}
+
+$(watchForm);
